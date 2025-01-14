4ed6fcf8a68ee42bbe0600fb7827b4c0330f7e26
xisus90 <xisus90@gmail.com>
2025-01-14 21:08:11 +0100

    refactor code

--- scraper_stock/scraper_code.py
+++ scraper_stock/scraper_code.py
@@ -1,28 +1,12 @@
 import re
 import requests
-#from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 
-
-class SearchGame:
-
-    def __init__(self, game, platform):       
-            self._game = game
-            self._platform = platform
-
-    def search(self):
-
-        search_platform = LookingDataPlatform(self._platform).looking_platforms()
-        price_game = SearchPrice(self._game, search_platform).Lookingdataprice()
-        return price_game
-        
 
 
 class LookingDataPlatform:
-        
-    def __init__(self, platform):
-        self._platform = platform
-        
-    def looking_platforms(self):
+    
+    def looking_platforms(self, platform):
         data_platforms = {
                         "Battle.net": "https://www.instant-gaming.com/es/busquedas/?platform%5B%5D=&type%5B%5D=battle.net&sort_by=&min_reviewsavg=10&max_reviewsavg=100&noreviews=1&min_price=0&max_price=200&noprice=1&gametype=all&search_tags=0&query=",
                         "Epic Games": "https://www.instant-gaming.com/es/busquedas/?platform%5B%5D=&type%5B%5D=epic+games&sort_by=&min_reviewsavg=10&max_reviewsavg=100&noreviews=1&min_price=0&max_price=200&noprice=1&gametype=all&search_tags=0&query=",
@@ -35,21 +19,25 @@ class LookingDataPlatform:
                         "Steam":"https://www.instant-gaming.com/es/busquedas/?platform%5B%5D=&type%5B%5D=steam&sort_by=&min_reviewsavg=10&max_reviewsavg=100&noreviews=1&min_price=0&max_price=200&noprice=1&gametype=all&search_tags=0&query=",
                         "Ubisoft":"https://www.instant-gaming.com/es/busquedas/?platform%5B%5D=&type%5B%5D=ubisoft+connect&sort_by=&min_reviewsavg=10&max_reviewsavg=100&noreviews=1&min_price=0&max_price=200&noprice=1&gametype=all&search_tags=0&query="
                         }
-        platform_link = data_platforms[self._platform]
+        platform_link = data_platforms[platform]
         
         return platform_link
 
 
+class SearchGame:
 
-class SearchPrice():
+    def __init__(self, game, platform):       
+            self._game = game
+            self._platform = platform
+
+    def search(self):
+        search_platform = LookingDataPlatform().looking_platforms(self._platform)
+        price_game = self.Lookingdataprice()
+        return price_game
         
-        def __init__(self, game, platform):
-             self._game = game
-             self._platform = platform
-
-        def Lookingdataprice(self):
-            data_url = f"{self._platform}{self._game}"
-            result = requests.get(data_url)
-            content = result.text
-            return content
+    def Lookingdataprice(self):
+        data_url = f"{self._platform}{self._game}"
+        result = requests.get(data_url)
+        content = result.text
+        return content
              
