225bb3a7e1cd3fe91b8c80bdf5ace0424590ada8
xisus90 <xisus90@gmail.com>
2025-03-05 17:32:27 +0100

    add function

--- qr/qr_code-py.py
+++ qr/qr_code-py.py
@@ -1,59 +1,109 @@
 import pandas as pd
 import qrcode
 import os
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+
+class QR:
+
+    def __init__(self, file_path="exported_data.csv"):
+            """Inicializa el DataLoader y carga los datos desde un archivo CSV/Excel."""
+            self._file_path = file_path
+            self._df = self.load_data()
+
+    def load_data(self):
+        """Carga los datos del archivo en un DataFrame de Pandas."""
+
+        try:
+            df = pd.read_csv(self._file_path, delimiter='\t', encoding='utf-8')
+            print(f"✅ Archivo {self._file_path} cargado correctamente.")
+            return df
+        except FileNotFoundError:
+            print(f"❌ Error: El archivo {self._file_path} no se encontró.")
+            return None
+        except Exception as e:
+            print(f"❌ Error al leer el archivo: {e}")
+            return None
 
 
-class Falla_QR_Mail():
+    def Sendmail(self, mails, file_name):
+        """Envía un correo a los usuarios suscritos cuando el precio de un juego baja."""
+        
+        SMTP_SERVER = "smtp.gmail.com"
+        SMTP_PORT = 587
+        EMAIL_SENDER = "krukasd90@gmail.com"  
+        EMAIL_PASSWORD = "swvh utym onkh gzxp"
 
-    def __init__(self):
-        pass        
+        if isinstance(mails, str):  
+            mails = [mails]  
 
-    def send_mail(self, mail):
+        subject = "Código QR para comida de la falla"
+        body = f"""        Hola, aquí tienes el código QR para la falla.       """
 
-        print(f"el correo es {mail}")
+        msg = MIMEMultipart()
+        msg["From"] = EMAIL_SENDER
+        msg["To"] = ", ".join(mails)
+        msg["Subject"] = subject
+        msg.attach(MIMEText(body, "plain")) 
 
 
+        file_path = os.path.abspath(os.path.join("QR_with_mail", file_name))
 
-    def QR(self):
+        with open(file_path, "rb") as attachment:  # ✅ Usa la ruta completa
+            part = MIMEBase("application", "octet-stream")
+            part.set_payload(attachment.read())
+
+        encoders.encode_base64(part)
+        part.add_header("Content-Disposition", f"attachment; filename={file_name}")
+
+        msg.attach(part)
+
+        try:
+            server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+            server.starttls() 
+            server.login(EMAIL_SENDER, EMAIL_PASSWORD) 
+
+            server.sendmail(EMAIL_SENDER, mails, msg.as_string())
+
+            print(f"✅ Correo enviado correctamente. {mails}")
+
+        except Exception as e:
+            print(f"❌ Error al enviar el correo: {e}")
+
+        finally:
+            server.quit() 
+
+
+    def QR_generate(self):
         
         directory_script = os.path.dirname(os.path.abspath(__file__))
         os.chdir(directory_script)
-
-        file = "datos_falla.csv"
 
         destiny_Carpet_mail = "QR_with_mail"
         destiny_Carpet_no_mail = "QR_Without_mail"
         os.makedirs(destiny_Carpet_mail, exist_ok=True)
         os.makedirs(destiny_Carpet_no_mail, exist_ok=True)
 
-        try:
-            df = pd.read_csv(file, delimiter='\t', encoding='utf-8')
-        except Exception as e:
-            print(f"Error al leer el archivo CSV: {e}")
-            exit()
-
-        print(f"Columnas detectadas antes de limpieza: {df.columns.tolist()}")
-
-        df.columns = df.columns.str.strip().str.lower().str.replace(r'\s+', ' ', regex=True)
-
-        print(f"Columnas detectadas después de limpieza: {df.columns.tolist()}")
+        #print(f"Columnas detectadas antes de limpieza: {self._df.columns.tolist()}")
+        self._df.columns = self._df.columns.str.strip().str.lower().str.replace(r'\s+', ' ', regex=True)
 
         colum_needed = ["nombre", "telefono", "mail"]
         for col in colum_needed:
-            if col not in df.columns:
+            if col not in self._df.columns:
                 print(f"Error: Falta la columna '{col}' en el archivo CSV.")
                 exit()
 
         count_mails = 0
         count_no_mails = 0
 
-        for fila in range(len(df)):
-            name = str(df["nombre"].iloc[fila]).strip()
-            phone = str(df["telefono"].iloc[fila]).strip()
-            mail = str(df["mail"].iloc[fila]).strip()
+        for line in range(len(self._df)):
+            name = str(self._df["nombre"].iloc[line]).strip()
+            mail = str(self._df["mail"].iloc[line]).strip()
 
-            data = f"Nombre: {name}, Teléfono: {phone}, Correo: {mail}"
-
+            data = f"Nombre: {name}"
 
             qr = qrcode.QRCode(
                 version=1,
@@ -66,22 +116,20 @@ class Falla_QR_Mail():
 
             img = qr.make_image(fill="black", back_color="white")
 
-            file_name = "".join(c if c.isalnum() or c in " _-" else "_" for c in name)
-
             if mail == "nan":
-                img.save(os.path.join(destiny_Carpet_no_mail, f"codigo_qr_{file_name}.png"))
+                img.save(os.path.join(destiny_Carpet_no_mail, f"codigo_qr_{name}.png"))
                 count_no_mails += 1
             if mail != "nan":
                 count_mails += 1
-                mails = self.send_mail(mail)
-                img.save(os.path.join(destiny_Carpet_mail, f"codigo_qr_{file_name}.png"))
-        
+                img.save(os.path.join(destiny_Carpet_mail, f"codigo_qr_{name}.png"))
+                file_name = f"codigo_qr_{name}.png"
+                self.Sendmail(mail, file_name)
+
         total_count = count_no_mails + count_mails
         print (f"se han guardado un total de {count_mails} QR con mails y {count_no_mails} QR sin mails, total {total_count}")
 
             
 
-
 if __name__ == "__main__":
-    Falla_QR_Mail().QR()
+    QR().QR_generate()
 
